66170becf206cdfe835e16127ea0b38c71134a6b
User <User@DESKTOP-HSEQPU2>
2024-02-12 13:08:43 +0400

    Добавьте файлы проекта.

---
+++ Interest-Bearing Deposit/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interest_Bearing_Deposit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float money;
+            int years;
+            int procent;
+
+            Console.Write("Введите количество средств, внесеннных на вклад: ");
+            money = Convert.ToSingle(Console.ReadLine());
+            Console.Write("На сколько лет открыт вклад? ");
+            years = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Под какой процент открыт вклад? ");
+            procent = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 1; i <= years; i++)
+            {
+                money += money * procent / 100;
+                Console.WriteLine($"За: {i} год у вас: {money} средств ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Interest-Bearing Deposit/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Interest-Bearing Deposit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Interest-Bearing Deposit")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("c58af4b2-4362-403e-b430-56970d23612a")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
